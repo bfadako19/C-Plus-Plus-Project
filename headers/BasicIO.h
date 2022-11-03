e001e673352700e60e93823dd48ffdfba423fd1d
Bryce Fadako <fadakob@chc.edu>
2022-11-03 10:47:11 -0400

    11/3/22

---
+++ headers/BasicIO.h
@@ -0,0 +1,59 @@
+//include pre-processor directives
+#include <iostream>
+#include <string>
+#define FIRST_NAME "Bryce"
+#define LAST_NAME "Fadako"
+#define AGE 21
+#define WEIGHT 170
+
+//use C++ standard library namespace
+using namespace std;
+
+/*this function will use cout and the insertion operator to write data to stdout*/
+void output (void){
+    //cout is an object of the output stream of a C++ application (terminal,file)
+    //<< is the insertion operator and its used to show output to the output stream
+    //a newline character is not appended to the end of th output, so we must use
+    // the endl command
+    cout << "Your first name is " << FIRST_NAME << endl;
+    cout << "Your last name is " << LAST_NAME << endl;
+    cout << "Your weight is " << WEIGHT << " and your age is "<< AGE << endl;
+    cout << FIRST_NAME << " ";
+    cout << LAST_NAME << endl;
+}
+
+/*this function will use cin and the extraction operator to input data from stdin*/
+void input(){
+    //declare variables to store input data
+    int age;
+    float weight;
+    //C++ has the string type
+    string name;
+    //input an int from stdin
+    cout << "Enter your age: ";
+    //cin is an object of the input stream of C++ application 
+    //>> is the extraction operator and its used to read input from the input
+    //stream and place it in a variable
+    cin >> age;
+    cout << "Entered age is: " << age << endl;
+    //input a float from stdin
+    cout << "Enter your weight: ";
+    cin >> weight;
+    cout << "Entered weight is:" << weight << endl;
+    //input a float and an int
+    cout << "Enter your weight and age seperated by a space: ";
+    cin >> weight >> age;
+    cout << "Entered weight is:" << weight << " and entered age is: " << age << endl;
+    //input a string
+    cout << "Enter your full name: ";
+    //cin stops reading when it encounters an empty space
+    //cin >> name ;
+    //getline reads a full line from the input stream, including the newline character
+    // generated when the enter key is pushed. This can prevent it from reading the actual
+    //input string, so we must call the ignore function before calling getline to ignore the 
+    //newline character
+    cin.ignore();
+    getline(cin,name);
+    cout << "Entered full name is: " << name << endl;
+
+}
